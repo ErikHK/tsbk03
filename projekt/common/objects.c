09215666d278244a9abf4136687fbf9f8be428c9
ErikHK <erikhk@gmail.com>
2015-10-17 18:25:50 +0200

    added objects.ch files

---
+++ projekt/common/objects.c
@@ -0,0 +1,166 @@
+#include "objects.h"
+#include <stdlib.h>
+
+void create_cow(cow_s * c)
+{
+  c->main_body = LoadModelPlus("./res/ko_fine.obj");
+  c->matrix = S(.1,.1,.1); //makes it roughly 5 high
+  c->pos = SetVector(0,0,0);
+  c->speed = SetVector(0,0,0);
+  c->acc = SetVector(0,0,0);
+  c->force = SetVector(0,0,0);
+  c->torque = SetVector(0,0,0);
+  c->momentum = SetVector(0,0,0);
+  c->mass = 2.0;
+  c->angle = 0;
+  c->d_angle = 0;
+  c->jumping = 0;
+}
+
+void update_cow(cow_s * c, GLfloat dT)
+{
+  vec3 dP, dX;
+
+  dP = ScalarMult(c->force, dT);
+  c->momentum = VectorAdd(c->momentum, dP);
+  c->speed = ScalarMult(c->momentum, 1.0/c->mass);
+
+  dX = ScalarMult(c->speed, dT);
+
+  c->pos = VectorAdd(c->pos, dX);
+
+  if(c->pos.y <= 0)
+  {
+    c->pos.y=0;// = SetVector(c->pos.x, 0, c->pos.z);
+    c->jumping = 0;
+    c->momentum.y = 0;
+    c->force.y=0;// = SetVector(c->force.x, 0, c->force.z);
+  }
+
+  printf("%f\n", c->pos.y);
+
+
+
+  //c->matrix = Mult(S(.1, .1, .1), T(c->pos.x, c->pos.y, c->pos.z));
+}
+
+void move_cow(cow_s * c, float angle)
+{
+  if(keyIsDown(0x20))
+  {
+    c->momentum.y = 20;
+    c->jumping = 1;
+  }
+
+  if(keyIsDown('p') && Norm(c->speed) < 15)
+  {
+    c->force = SetVector(-40*cos(angle),0,-40*sin(angle));
+    //printf("wtf");
+  }
+  else if(keyIsDown('u') && Norm(c->speed) < 15)
+    c->force = SetVector(40*cos(angle),0,40*sin(angle));
+  else
+  {
+    //c->force = SetVector(0,0,0);
+    c->force = SetVector(-c->momentum.x*3,0,-c->momentum.z*3);
+  }
+
+  if(c->pos.y > 0 && c->jumping)
+    c->force = SetVector(0,-40,0);
+
+
+/*
+  if(c->pos.y < 0)
+  {
+    c->pos.y=0;// = SetVector(c->pos.x, 0, c->pos.z);
+    c->jumping = 0;
+    c->force.y=0;// = SetVector(c->force.x, 0, c->force.z);
+  }
+*/
+  printf("%f t\n", c->pos.y);
+
+  //printf("%f\n", c->speed.x);
+}
+
+void update_floor(floor_s * f, cow_s * c)
+{
+  f->matrix = T(-c->pos.x, -c->pos.y, -c->pos.z);
+//                   ArbRotate(SetVector(0,1,0), c->angle));
+
+}
+
+void create_floor(floor_s * f)
+{
+  f->model = LoadModelPlus("./res/ground.obj");
+  LoadTGATextureSimple("./res/golv.tga", &(f->tex));
+}
+
+void draw_floor(floor_s * f, GLuint program)
+{
+  //f->matrix = S(2,2,2);
+  glUniformMatrix4fv(glGetUniformLocation(program, "mdl_matrix"), 1, GL_TRUE, f->matrix.m);
+  //glBindTexture(GL_TEXTURE_2D, f->tex);
+  DrawModel(f->model, program, "inPosition", "inNormal", "inTexCoord");
+
+}
+
+void turn_cow(cow_s * c, float angle)
+{
+  float diff = c->angle - angle;
+  c->matrix = Mult(S(.1,.1,.1), ArbRotate(SetVector(0,1,0), c->angle));
+  c->angle -= diff/10;
+
+}
+
+void create_bone(bone_s * b, vec3 pos)
+{
+  b->pos = pos;
+  b->body = LoadModelPlus("./res/groundsphere.obj");
+  //b->body = LoadModelPlus("./res/ko_rough.obj");
+  b->matrix = Mult(T(pos.x, pos.y, pos.z), S(.1, .1, .1));
+  //b->matrix = S(3, 3, 3);
+  //b->matrix = T(0,0,0);
+
+}
+
+void create_joint(joint_s * j, vec3 pos, char * Mvar, char * posvar, char * boneposvar, int id)
+{
+  j->isnull = 1;
+  j->Mvar = Mvar;
+  j->posvar = posvar;
+  j->boneposvar = boneposvar;
+
+  j->pos = pos;
+  j->id = id;
+  j->body = LoadModelPlus("./res/groundsphere.obj");
+  j->body_matrix = Mult(T(pos.x, pos.y, pos.z), S(.1, .1, .1));
+  j->T = T(pos.x, pos.y, pos.z);
+  j->M = T(pos.x, pos.y, pos.z);
+  j->Mp = T(pos.x, pos.y, pos.z);
+  j->Mtot = T(pos.x, pos.y, pos.z);
+  //j->Minv = InvertMat4(j->M);
+  j->Minv = InvertMat4(T(pos.x, pos.y, pos.z));
+  j->R = T(0,0,0);
+  //j->body_matrix = S(.1, .1, .1);
+  //j->body_matrix = IdentityMatrix();
+}
+
+void draw_cow(cow_s * c, GLuint program)
+{
+  glUniformMatrix4fv(glGetUniformLocation(program, "mdl_matrix"), 1, GL_TRUE, c->matrix.m);
+  DrawModel(c->main_body, program, "inPosition", "inNormal", "inTexCoord");
+}
+
+void draw_bone(bone_s * b, GLuint program)
+{
+  glUniformMatrix4fv(glGetUniformLocation(program, "mdl_matrix"), 1, GL_TRUE, b->matrix.m);
+  DrawModel(b->body, program, "inPosition", "inNormal", "inTexCoord");
+
+}
+
+void draw_joint(joint_s *j, GLuint program)
+{
+  glUniformMatrix4fv(glGetUniformLocation(program, "mdl_matrix"), 1, GL_TRUE, j->body_matrix.m);
+  DrawModel(j->body, program, "inPosition", "inNormal", "inTexCoord");
+  
+}
---
+++ projekt/common/objects.h
@@ -0,0 +1,78 @@
+#include "loadobj.h"
+#include "VectorUtils3.h"
+#include "GL_utilities.h"
+#include "LoadTGA.h"
+
+#define MAX_CHILDREN	4
+
+typedef struct cow_t
+{
+  Model * main_body;
+  Model * udder;
+  mat4 matrix;
+
+  vec3 pos;
+  vec3 speed;
+  vec3 acc;
+  vec3 force;
+  vec3 torque;
+  vec3 momentum;
+  float mass;
+  float angle;
+  float d_angle;
+  int jumping;
+  GLuint body_tex;
+
+} cow_s;
+
+
+typedef struct floor_t
+{
+  Model * model;
+  mat4 matrix;
+  GLuint tex;
+} floor_s;
+
+typedef struct bone_t
+{
+  Model * body;
+  vec3 orig_pos;
+  vec3 pos;
+  mat4 matrix;
+
+} bone_s;
+
+typedef struct joint_t
+{
+  Model * body;
+  vec3 orig_pos;
+  vec3 pos;
+  mat4 body_matrix;
+  mat4 T;
+  mat4 R;
+  mat4 M;
+  mat4 Mp;
+  mat4 Minv;
+  mat4 Mtot;
+  mat4 tmp;
+  int id;
+  struct joint_t *parent;
+  struct joint_t *child[MAX_CHILDREN];
+  char * Mvar;
+  char * posvar;
+  char * boneposvar;
+  int isnull;
+
+} joint_s;
+
+
+void create_cow();
+void draw_cow();
+void turn_cow(cow_s * c, float angle);
+void update_cow(cow_s * c, GLfloat dT);
+void update_floor(floor_s * f, cow_s * c);
+void move_cow(cow_s * c, float angle);
+void create_bone();
+void draw_bone();
+void create_joint();
+void draw_joint();
